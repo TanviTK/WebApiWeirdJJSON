1ddfbeeb35e201a3349e47a36149869067bb1054
tanvi khatib <tanvikhatib10@outlook.com>
2021-04-24 23:33:58 -0400

    Add project files.

---
+++ WebApiWeirdJJSON/Model/WeatherOutput1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiWeirdJJSON.Model
+{
+    public class WeatherOutput1
+    {
+        public string city { get; set; }
+        public string _date { get; set; }
+        public WeatherTemperature temperatureInfo { get; set; }
+    }
+}
---
+++ WebApiWeirdJJSON/Model/WeatherTemperature.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiWeirdJJSON.Model
+{
+    public class WeatherTemperature
+    {
+        public double min_temp { get; set; }
+        public double max_temp { get; set; }
+    }
+}
